97ac48d4dfdb80362fb847eec2ae24f6d0f4ae81
Hariprasath R <hariprasath1115@gmail.com>
2023-11-19 14:20:04 +0530

    day 6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,41 @@
-
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+    navClose = document.getElementById('nav-close');
 
-
-
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
 }
 
-
-
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
 }
 
+const navLink = document.querySelectorAll('.nav__link');
 
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
+    skillsHeader = document.querySelectorAll('.skills__header');
 
-function toggleSkiills(){
-    let itemClass = this.parentNode.className
-
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
+function toggleSkills() {
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
     }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
+
+    if (this.parentNode.className === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
     }
 }
 
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click', toggleSkiills)
-})
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
